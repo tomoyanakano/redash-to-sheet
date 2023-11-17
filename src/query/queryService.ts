f23bae5e43dc576666ae9d664d69de29dc675354
tomoyanakano <tomoyanakano>
2023-11-17 14:40:40 +0900

    :bulb: Add comments to service class methods

--- src/query/queryService.ts
+++ src/query/queryService.ts
@@ -11,16 +11,19 @@ export interface IQueryService {
 export class QueryService implements IQueryService {
   constructor(private redashService: IRedashService) {}
 
+  // Refreshes a query
   async refreshQuery(queryId: string, params: string = ""): Promise<any> {
     const response = await this.redashService.request("post", `/api/queries/${queryId}/refresh?${params}`);
     return JSON.parse(response);
   }
 
+  // Fetches a job status
   async fetchJob(jobId: string): Promise<any> {
     const response = await this.redashService.request("get", `/api/jobs/${jobId}`);
     return JSON.parse(response).job;
   }
 
+  // Polls a job until it's completed
   async pollingJob(jobId: string): Promise<any> {
     let job = await this.fetchJob(jobId);
     while (job.status !== 3) {
@@ -33,11 +36,13 @@ export class QueryService implements IQueryService {
     return job;
   }
 
+  // Fetches the result of a query
   async fetchQueryResult(queryResultId: string): Promise<any> {
     const response = await this.redashService.request("get", `/api/query_results/${queryResultId}.csv`);
     return Utilities.parseCsv(response);
   }
 
+  // Fetches a query and returns its result
   async fetchQuery(queryId: string, params: string = ""): Promise<any> {
     const response = await this.refreshQuery(queryId, params);
     const job = await this.pollingJob(response.job.id);

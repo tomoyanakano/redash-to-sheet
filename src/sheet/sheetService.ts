e66943066a8ec5710918d8b7703085381f357959
tomoyanakano <tomo1999724@gmail.com>
2023-11-22 14:56:06 +0900

    :sparkles: Add isSheetEmpty method to SheetServiec

--- src/sheet/sheetService.ts
+++ src/sheet/sheetService.ts
@@ -7,6 +7,17 @@ export interface ISheetService {
 }
 
 export class SheetService implements ISheetService {
+  // check if the sheet is empty
+  private isSheetEmpty(sheet: GoogleAppsScript.Spreadsheet.Sheet) {
+    const data = sheet.getDataRange().getValues();
+    // データが空かどうかをチェック
+    if (data.length == 1 && data[0].length == 1 && data[0][0] === "") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   // Update or append values in a given sheet. Rows are identified by a key in a specified column.
   public updateValues(
     sheet: GoogleAppsScript.Spreadsheet.Sheet,
@@ -19,9 +30,9 @@ export class SheetService implements ISheetService {
     const keyValues = sheet
       .getRange(1, keyColumnIndex, lastRowIndex)
       .getValues();
-    const sheetValues = sheet
-      .getRange(1, 1, lastRowIndex, lastColumnIndex)
-      .getValues();
+    const sheetValues = this.isSheetEmpty(sheet)
+      ? []
+      : sheet.getRange(1, 1, lastRowIndex, lastColumnIndex).getValues();
 
     const keyIndexMap = new Map();
     keyValues.forEach((keyValue, index) => {
